59cb7b507535c4f7a6a1600dbb296c5957f3bba9
Mosabbir Khan <mosabbir.khan@optimizely.com>
2023-08-08 12:48:12 +0600

    Account screen shows menus

--- lib/src/pages/storefront/destinations/account_destination.dart
+++ lib/src/pages/storefront/destinations/account_destination.dart
@@ -1,13 +1,115 @@
 import 'package:flutter/material.dart';
 
 class AccountDestination extends StatelessWidget {
-  const AccountDestination({super.key});
+  AccountDestination({super.key});
+
+  final menuItemsList = [
+    'Quick Order',
+    'Location Finder',
+    'Shop Brands',
+    'Shop Categories en',
+    'View Account on Website',
+    'Settings'
+  ];
+
+  final List<IconData> menuIconsList = [
+    Icons.touch_app,
+    Icons.location_on_outlined,
+    Icons.store_outlined,
+    Icons.sell_outlined,
+    Icons.explore_outlined,
+    Icons.settings_outlined,
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Account')),
-      body: const Center(child: Text('Account')),
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(
+              width: double.infinity,
+              height: 150,
+              child: Card(
+                elevation: 2,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(12)),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                          'To view previous orders, lists and other features, please sign in to your acount.'),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      FilledButton(
+                        onPressed: () {},
+                        child: const SizedBox(
+                          width: double.infinity,
+                          child: Center(child: Text('SIGN IN')),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Expanded(
+                child: _MenuItemsListView(
+                    menuItemsList: menuItemsList,
+                    menuIconsList: menuIconsList)),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _MenuItemsListView extends StatelessWidget {
+  const _MenuItemsListView(
+      {required this.menuItemsList, required this.menuIconsList});
+
+  final List<String> menuItemsList;
+  final List<IconData> menuIconsList;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      itemCount: menuItemsList.length,
+      itemBuilder: (context, index) {
+        return Padding(
+          padding:
+              const EdgeInsets.only(top: 10, bottom: 10, left: 20, right: 20),
+          child: SizedBox(
+            width: double.infinity,
+            child: Row(
+              children: [
+                Icon(
+                  menuIconsList[index],
+                  color: index >= 4
+                      ? Theme.of(context).colorScheme.onSurface
+                      : Theme.of(context).colorScheme.primary,
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                Text(menuItemsList[index]),
+              ],
+            ),
+          ),
+        );
+      },
+      separatorBuilder: (context, index) => const Padding(
+        padding: EdgeInsets.only(left: 20, right: 20),
+        child: Divider(),
+      ),
     );
   }
 }
