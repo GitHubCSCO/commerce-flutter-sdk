9b3415844194fe4d1764c1946a756302c09e60fb
Mosabbir Khan <mosabbir.khan@optimizely.com>
2023-08-16 10:29:19 +0600

    BaseModel Model

---
+++ packages/commerce_dart_sdk/lib/src/models/base_model.dart
@@ -0,0 +1,6 @@
+part of commerce_dart_sdk.models;
+
+abstract class BaseModel {
+  late String uri;
+  late Properties properties;
+}
--- packages/commerce_dart_sdk/lib/src/models/models.dart
+++ packages/commerce_dart_sdk/lib/src/models/models.dart
@@ -1,3 +1,4 @@
 library commerce_dart_sdk.models;
 
 part 'properties.dart';
+part 'base_model.dart';
