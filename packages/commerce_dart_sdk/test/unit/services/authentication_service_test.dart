76be55f5c7e7da8cda59f4b84c1633b64f8131bf
Mosabbir Khan <mosabbir.khan@optimizely.com>
2023-09-16 23:31:32 +0600

    removed unnecessary string in authentication_service_test.dart

--- packages/commerce_dart_sdk/test/unit/services/authentication_service_test.dart
+++ packages/commerce_dart_sdk/test/unit/services/authentication_service_test.dart
@@ -41,9 +41,6 @@ class MockSecureStorageService implements ISecureStorageService {
 }
 
 void main() {
-  String jsonString =
-      '{"uri":"https://mobilespire.commerce.insitesandbox.com/api/v1/warehouses/126729d8-8e2b-48f3-94fe-af42004af05b","id":"126729d8-8e2b-48f3-94fe-af42004af05b","name":"Banani","address1":"Green Grandeur (4th ,10th and 13th floor), Plot 58/E, Kemal Ataturk Avenue, Dhaka","address2":"","city":"Dhaka","contactName":"","countryId":"b1ab0470-e310-e311-ba31-d43d7e4e88b2","deactivateOn":null,"description":"Banani W","phone":"","postalCode":"1213","shipSite":"","state":"","isDefault":false,"alternateWarehouses":[],"latitude":0.000000,"longitude":0.000000,"hours":"","distance":0.0,"allowPickup":false,"pickupShipViaId":null,"properties":{}}';
-
   IClientService clientService = ClientService(
     localStorageService: MockLocalStorageService(),
     secureStorageService: MockSecureStorageService(),
