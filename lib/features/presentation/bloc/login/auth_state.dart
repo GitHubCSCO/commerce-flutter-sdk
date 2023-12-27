07ca1e2af414cb224499d58380f4930792057024
Anisur Rahaman <>
2023-12-27 20:31:18 +0600

    test case added for login bloc and login usecase

--- lib/features/presentation/bloc/login/auth_state.dart
+++ lib/features/presentation/bloc/login/auth_state.dart
@@ -1,24 +1,45 @@
 import 'package:commerce_flutter_app/features/domain/enums/auth_status.dart';
+import 'package:equatable/equatable.dart';
 
-abstract class AuthenticationState {
+abstract class AuthenticationState extends Equatable {
   const AuthenticationState();
 }
 
-class LoginInitialState extends AuthenticationState {}
+class LoginInitialState extends AuthenticationState {
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+}
 
-class LoginLoadingState extends AuthenticationState {}
+class LoginLoadingState extends AuthenticationState {
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+}
 
 class LoginSuccessState extends AuthenticationState {
   const LoginSuccessState();
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
 }
 
 class LoginFailureState extends AuthenticationState {
   final String error;
 
   const LoginFailureState(this.error);
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
 }
 
-class LogoutLoadingState extends AuthenticationState {}
+class LogoutLoadingState extends AuthenticationState {
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+}
 
 class AuthenticationAuthState extends AuthenticationState {
   final AuthStatus status;
@@ -28,4 +49,8 @@ class AuthenticationAuthState extends AuthenticationState {
       : this(status: AuthStatus.authenticated);
   const AuthenticationAuthState.unauthenticated()
       : this(status: AuthStatus.unauthenticated);
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
 }
