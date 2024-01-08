3a5524cc42c88e276401c02c3829320ac241d0d9
Mosabbir-Khan <mosabbir.khan@optimizely.com>
2024-01-08 13:12:01 +0600

    add: initial welcome screen design

--- lib/features/presentation/screens/welcome/welcome_screen.dart
+++ lib/features/presentation/screens/welcome/welcome_screen.dart
@@ -1,4 +1,5 @@
 import 'package:commerce_flutter_app/core/constants/app_route.dart';
+import 'package:commerce_flutter_app/core/constants/localization_constants.dart';
 import 'package:flutter/material.dart';
 
 class WelcomeScreen extends StatelessWidget {
@@ -6,23 +7,122 @@ class WelcomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
+    return const Scaffold(
+      backgroundColor: Colors.yellow,
+      body: WelcomeFirstPage(),
+    );
+  }
+
+  // Text("Welcome to Optimizely B2B Mobile App")
+}
+
+class WelcomeFirstPage extends StatelessWidget {
+  const WelcomeFirstPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: WelcomeCard(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            const Text("Welcome to Optimizely B2B Mobile App"),
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              LocalizationKeyword.firstTimeWelcome,
+              style: TextStyle(
+                fontSize: 33,
+                fontWeight: FontWeight.w200,
+              ),
+            ),
+            const SizedBox(height: 15),
+            const Text(
+              'This app enables Optimizely customers to easily access their B2B Commerce application.',
+              style: TextStyle(
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+            const SizedBox(height: 15),
+            const Text(
+              'If you\'re new to Optimizely and would like access to the mobile app version of your storefront, visit our website.',
+              style: TextStyle(
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+            const SizedBox(height: 15),
+            const Text(
+              'Sign in below if you are an existing customer.',
+              style: TextStyle(
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+            const Expanded(child: SizedBox()),
+            FilledButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(
+                  Colors.deepPurple,
+                ),
+                shape: MaterialStateProperty.all(
+                  RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(0)),
+                ),
+              ),
+              child: const SizedBox(
+                width: double.infinity,
+                child: Center(
+                  child: Text(
+                    LocalizationKeyword.signInPrompt,
+                  ),
+                ),
+              ),
+            ),
             ElevatedButton(
-                onPressed: () {
-                  AppRoute.shop.navigate(context);
-                },
-                child: const Text("Go to B2B Store")),
+              onPressed: () {},
+              style: ButtonStyle(
+                elevation: MaterialStateProperty.all(0),
+                shape: MaterialStateProperty.all(
+                  RoundedRectangleBorder(
+                    side: const BorderSide(color: Colors.deepPurple),
+                    borderRadius: BorderRadius.circular(0),
+                  ),
+                ),
+                foregroundColor: MaterialStateProperty.all(
+                  Colors.deepPurple,
+                ),
+              ),
+              child: const SizedBox(
+                width: double.infinity,
+                child: Center(
+                  child: Text(LocalizationKeyword.visitWebsite),
+                ),
+              ),
+            ),
           ],
         ),
       ),
     );
   }
+}
 
-  // Text("Welcome to Optimizely B2B Mobile App")
+class WelcomeCard extends StatelessWidget {
+  const WelcomeCard({super.key, this.child});
+
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+      constraints: const BoxConstraints(maxWidth: 360, maxHeight: 500),
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(0),
+        ),
+        elevation: 0,
+        color: const Color.fromRGBO(255, 255, 255, 0.8),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+          child: child,
+        ),
+      ),
+    );
+  }
 }
