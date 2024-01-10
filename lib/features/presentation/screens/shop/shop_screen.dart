8e3cfc8d28b341d61c4b39092cfc8a8b198eb1e3
Anisur Rahaman <>
2024-01-10 17:10:08 +0600

    shop page refactor

--- lib/features/presentation/screens/shop/shop_screen.dart
+++ lib/features/presentation/screens/shop/shop_screen.dart
@@ -1,8 +1,8 @@
-import 'package:commerce_flutter_app/features/presentation/widget/linkgrid_section_widget.dart';
-import 'package:commerce_flutter_app/features/presentation/widget/linklist_section_widget.dart';
+import 'package:commerce_flutter_app/features/presentation/widget/action_grid_section_widget.dart';
+import 'package:commerce_flutter_app/features/presentation/widget/action_list_section_widget.dart';
 import 'package:commerce_flutter_app/features/presentation/widget/product_carousel_section_widget.dart';
 import 'package:commerce_flutter_app/features/presentation/widget/search_history_section_widget.dart';
-import 'package:commerce_flutter_app/features/presentation/widget/slideshow_section_widget.dart';
+import 'package:commerce_flutter_app/features/presentation/widget/carousel_section_widget.dart';
 import 'package:flutter/material.dart';
 
 class ShopScreen extends StatelessWidget {
@@ -10,16 +10,21 @@ class ShopScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
-        children: const [
-          SlideShowSectionWidget(),
-          LinkGridSectionWidget(),
-          ProductCarouselSectionWidget(title: 'Top Sellers'),
-          ProductCarouselSectionWidget(title: 'VAT Products'),
-          ProductCarouselSectionWidget(title: 'VMI Products'),
-          LinkListSectionWidget(),
-          SearchHistorySectionWidget()
-        ],
+        children: getWidgets(),
       )
     );
   }
+
+  List<Widget> getWidgets() {
+    List<Widget> widgets = [];
+    widgets.add(CarouselSectionWidget());
+    widgets.add(ActionGridSectionWidget());
+    widgets.add(ProductCarouselSectionWidget(title: 'Top Sellers'));
+    widgets.add(ProductCarouselSectionWidget(title: 'VAT Products'));
+    widgets.add(ProductCarouselSectionWidget(title: 'VMI Products'));
+    widgets.add(ActionListSectionWidget());
+    widgets.add(SearchHistorySectionWidget());
+    return widgets;
+  }
+
 }
