179c84172ed7ae35596339e2f9e8dc4876dd94d2
Mosabbir-Khan <mosabbir.khan@optimizely.com>
2024-01-12 13:06:35 +0600

    add: domain selection cubit

--- lib/features/presentation/bloc/domain_selection/domain_selection_cubit.dart
+++ lib/features/presentation/bloc/domain_selection/domain_selection_cubit.dart
@@ -1,7 +1,49 @@
+import 'package:commerce_flutter_app/core/constants/localization_constants.dart';
+import 'package:commerce_flutter_app/features/domain/enums/domain_selection_status.dart';
+import 'package:commerce_flutter_app/features/domain/usecases/domain_selection_usecase/domain_selection_usecase.dart';
+import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'domain_selection_state.dart';
 
 class DomainSelectionCubit extends Cubit<DomainSelectionState> {
-  DomainSelectionCubit() : super(DomainSelectionInitial());
+  final DomainSelectionUsecase _domainSelectionUsecase;
+
+  DomainSelectionCubit(this._domainSelectionUsecase)
+      : super(DomainSelectionInitial());
+
+  Future<void> selectDomain(String domain) async {
+    emit(DomainSelectionInProgress());
+    final result = await _domainSelectionUsecase.domainSelectHandler(domain);
+
+    switch (result) {
+      case DomainSelectionStatus.success:
+        emit(DomainSelectionSuccess());
+        break;
+      case DomainSelectionStatus.failedOffline:
+        emit(
+          DomainSelectionFailedOffline(
+            'No Internet',
+            'Connection cannot be established.',
+          ),
+        );
+        break;
+      case DomainSelectionStatus.failedInvalidDomain:
+        emit(
+          DomainSelectionFailedInvalid(
+            LocalizationKeyword.invalidDomain,
+            LocalizationKeyword.domainWebsiteNotResponding,
+          ),
+        );
+        break;
+      case DomainSelectionStatus.failedMobileAppDisabled:
+        emit(
+          DomainSelectionFailedMobileAppDisabled(
+            LocalizationKeyword.mobileAppDisabled,
+            LocalizationKeyword.mobileAppDisabledDescription,
+          ),
+        );
+        break;
+    }
+  }
 }
--- lib/features/presentation/bloc/domain_selection/domain_selection_state.dart
+++ lib/features/presentation/bloc/domain_selection/domain_selection_state.dart
@@ -1,5 +1,50 @@
 part of 'domain_selection_cubit.dart';
 
-sealed class DomainSelectionState {}
+sealed class DomainSelectionState extends Equatable {}
 
-final class DomainSelectionInitial extends DomainSelectionState {}
+final class DomainSelectionInitial extends DomainSelectionState {
+  @override
+  List<Object?> get props => [];
+}
+
+final class DomainSelectionInProgress extends DomainSelectionState {
+  @override
+  List<Object?> get props => [];
+}
+
+final class DomainSelectionSuccess extends DomainSelectionState {
+  @override
+  List<Object?> get props => [];
+}
+
+final class _DomainSelectionFailed extends DomainSelectionState {
+  final String title;
+  final String message;
+
+  _DomainSelectionFailed(this.title, this.message);
+
+  @override
+  List<Object?> get props => [title, message];
+}
+
+final class DomainSelectionFailedOffline extends _DomainSelectionFailed {
+  DomainSelectionFailedOffline(super.title, super.message);
+
+  @override
+  List<Object?> get props => [title, message];
+}
+
+final class DomainSelectionFailedInvalid extends _DomainSelectionFailed {
+  DomainSelectionFailedInvalid(super.message, super.title);
+
+  @override
+  List<Object?> get props => [title, message];
+}
+
+final class DomainSelectionFailedMobileAppDisabled
+    extends _DomainSelectionFailed {
+  DomainSelectionFailedMobileAppDisabled(super.title, super.message);
+
+  @override
+  List<Object?> get props => [title, message];
+}
