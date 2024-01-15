406b29d15c419e4ce8ce1fcd58f0eb40753d0dae
Mosabbir-Khan <mosabbir.khan@optimizely.com>
2024-01-15 11:59:26 +0600

    add: color pallete

---
+++ lib/features/presentation/components/style.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppStyle {
+  static const Color primary500 = Color.fromRGBO(0, 55, 255, 1);
+  static const Color secondary800 = Color.fromRGBO(34, 34, 34, 1);
+  static const Color brand200 = Color.fromRGBO(179, 195, 255, 1);
+
+  static const Color neutral00 = Color.fromRGBO(255, 255, 255, 1);
+  static const Color neutral75 = Color.fromRGBO(245, 245, 245, 1);
+  static const Color newtural100 = Color.fromRGBO(237, 237, 237, 1);
+  static const Color neutral200 = Color.fromRGBO(214, 214, 214, 1);
+  static const Color neutral500 = Color.fromRGBO(158, 158, 158, 1);
+  static const Color neutral600 = Color.fromRGBO(112, 112, 112, 1);
+  static const Color neutral990 = Color.fromRGBO(34, 34, 34, 1);
+}
