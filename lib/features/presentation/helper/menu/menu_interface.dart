2615295e7b6f3c649d47165400440d364fff1cc1
Anisur Rahaman <>
2024-01-25 18:16:07 +0600

    bottom menu widget added

---
+++ lib/features/presentation/helper/menu/menu_interface.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+abstract class IMenu {
+  late String title;
+  late VoidCallback action;
+}
--- lib/features/presentation/screens/shop/shop_screen.dart
+++ lib/features/presentation/screens/shop/shop_screen.dart
@@ -1,6 +1,7 @@
 import 'package:commerce_flutter_app/features/presentation/base/base_dynamic_content_screen.dart';
 import 'package:commerce_flutter_app/features/presentation/bloc/shop/shop_page_bloc.dart';
 import 'package:commerce_flutter_app/features/presentation/bloc/shop/shop_page_state.dart';
+import 'package:commerce_flutter_app/features/presentation/widget/bottom_menu_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -9,25 +10,43 @@ class ShopScreen extends BaseDynamicContentScreen {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<ShopPageBloc, ShopPageState>(
-      builder: (context, state) {
-        switch(state) {
-          case ShopPageInitialState():
-          case ShopPageLoadingState():
-            return const Center(child: CircularProgressIndicator());
-          case ShopPageLoadedState():
-            return Scaffold(
-                body: ListView(
-                  children: buildContentWidgets(state.pageWidgets),
-                )
-            );
-          case ShopPageFailureState():
-            return const Center(child: Text('Failed Loading Shop'));
-          default:
-            return const Center(child: Text('Failed Loading Shop'));
-        }
-      },
+    return Scaffold(
+      appBar: AppBar(actions: <Widget>[
+        BottomMenuWidget(),
+      ]),
+      body: BlocBuilder<ShopPageBloc, ShopPageState>(
+        builder: (context, state) {
+          switch(state) {
+            case ShopPageInitialState():
+            case ShopPageLoadingState():
+              return const Center(child: CircularProgressIndicator());
+            case ShopPageLoadedState():
+              return Scaffold(
+                  body: ListView(
+                    children: buildContentWidgets(state.pageWidgets),
+                  )
+              );
+            case ShopPageFailureState():
+              return const Center(child: Text('Failed Loading Shop'));
+            default:
+              return const Center(child: Text('Failed Loading Shop'));
+          }
+        },
+      ),
     );
   }
+
+  // List<ToolMenu> _getToolMenu(BuildContext context) {
+  //   List<ToolMenu> list = [];
+  //   list.add(ToolMenu(
+  //     title: "Setting",
+  //     action: () {
+  //       ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+  //         content: Text("Sending Message"),
+  //       ));
+  //     }
+  //   ));
+  //   return list;
+  // }
 
 }
