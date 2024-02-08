7fb58c8b92415e0d2c15fc9422de77b0d52ebc39
Mosabbir-Khan <mosabbir.khan@optimizely.com>
2024-02-08 16:11:43 +0600

    add: inter-dependent string const for router

--- lib/core/constants/app_route.dart
+++ lib/core/constants/app_route.dart
@@ -3,16 +3,39 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+class RouteNames {
+  static const String welcome = 'welcome';
+  static const String domainSelection = 'domainSelection';
+  static const String login = 'login';
+  static const String shop = 'shop';
+  static const String search = 'search';
+  static const String account = 'account';
+  static const String cart = 'cart';
+  static const String productDetails = 'productDetails';
+  static const String checkout = 'checkout';
+}
+
+class RoutePaths {
+  static const String welcome = '/';
+  static const String domainSelection = '/${RouteNames.domainSelection}';
+  static const String login = '/${RouteNames.login}';
+  static const String shop = '/${RouteNames.shop}';
+  static const String search = '/${RouteNames.search}';
+  static const String account = '/${RouteNames.account}';
+  static const String cart = '/${RouteNames.cart}';
+  static const String checkout = '/${RouteNames.checkout}';
+}
+
 enum AppRoute {
-  welcome(name: 'welcome', fullPath: '/'),
-  domainSelection(name: 'domainSelection', fullPath: '/domainSelection'),
-  login(name: 'login', fullPath: '/login'),
-  shop(name: 'shop', fullPath: '/shop'),
-  search(name: 'search', fullPath: '/search'),
-  account(name: 'account', fullPath: '/account'),
-  cart(name: 'cart', fullPath: '/cart'),
-  productDetails(name: 'productDetails', fullPath: '/cart/:id'),
-  checkout(name: 'checkout', fullPath: '/checkout/:id');
+  welcome(name: RouteNames.welcome, fullPath: RoutePaths.welcome),
+  domainSelection(name: RouteNames.domainSelection, fullPath: RoutePaths.domainSelection),
+  login(name: RouteNames.login, fullPath: RoutePaths.login),
+  shop(name: RouteNames.shop, fullPath: RouteNames.shop),
+  search(name: RouteNames.search, fullPath: RoutePaths.search),
+  account(name: RouteNames.account, fullPath: RoutePaths.account),
+  cart(name: RouteNames.cart, fullPath: RoutePaths.cart),
+  productDetails(name: RouteNames.productDetails, fullPath: '${RoutePaths.cart}/:id'),
+  checkout(name: RouteNames.checkout, fullPath: '${RoutePaths.checkout}/:id');
 
   const AppRoute({
     required this.name,
