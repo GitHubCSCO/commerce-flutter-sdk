fdba5a8667e69d88ed989dcb2f8d813fbf9df980
Mosabbir-Khan <mosabbir.khan@optimizely.com>
2024-02-12 12:16:18 +0600

    add: account page banner with dummy name

--- lib/features/presentation/screens/account/account_screen.dart
+++ lib/features/presentation/screens/account/account_screen.dart
@@ -1,11 +1,13 @@
 import 'package:commerce_flutter_app/core/constants/app_route.dart';
 import 'package:commerce_flutter_app/core/constants/localization_constants.dart';
+import 'package:commerce_flutter_app/core/constants/site_message_constants.dart';
 import 'package:commerce_flutter_app/features/domain/enums/auth_status.dart';
 import 'package:commerce_flutter_app/core/injection/injection_container.dart';
 import 'package:commerce_flutter_app/features/presentation/base/base_dynamic_content_screen.dart';
 import 'package:commerce_flutter_app/features/presentation/bloc/account/account_page_bloc.dart';
 import 'package:commerce_flutter_app/features/presentation/bloc/auth/auth_cubit.dart';
 import 'package:commerce_flutter_app/features/presentation/components/buttons.dart';
+import 'package:commerce_flutter_app/features/presentation/components/style.dart';
 import 'package:commerce_flutter_app/features/presentation/cubit/logout/logout_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -34,44 +36,80 @@ class AccountPage extends BaseDynamicContentScreen {
             return const Center(child: CircularProgressIndicator());
           case AccountPageLoadedState():
             return Scaffold(
-                body: ListView(
-              children: [
-                BlocConsumer<AuthCubit, AuthState>(
-                  listener: (context, state) {
-                    context.read<AccountPageBloc>().add(AccountPageLoadEvent());
-                  },
-                  builder: (context, state) {
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20),
-                      child: state.status == AuthStatus.authenticated
-                          ? BlocListener<LogoutCubit, LogoutState>(
-                              listener: (context, state) {
-                                if (state is LogoutSuccess) {
-                                  context
-                                      .read<AuthCubit>()
-                                      .loadAuthenticationState();
-                                }
-                              },
-                              child: PrimaryButton(
-                                child:
-                                    const Text(LocalizationConstants.signOut),
-                                onPressed: () {
-                                  context.read<LogoutCubit>().logout();
-                                },
-                              ),
-                            )
-                          : PrimaryButton(
-                              child: const Text(LocalizationConstants.signIn),
-                              onPressed: () {
-                                AppRoute.login.navigateBackStack(context);
-                              },
-                            ),
-                    );
-                  },
-                ),
-                ...buildContentWidgets(state.pageWidgets),
-              ],
-            ));
+              appBar: AppBar(
+                backgroundColor: AppStyle.neutral00,
+                title: const Text(LocalizationConstants.account),
+                centerTitle: false,
+                automaticallyImplyLeading: false,
+              ),
+              body: ListView(
+                children: [
+                  Container(
+                    color: AppStyle.neutral00,
+                    width: double.infinity,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: AppStyle.defaultHorizontalPadding,
+                        vertical: AppStyle.defaultVerticalPadding,
+                      ),
+                      child: BlocConsumer<AuthCubit, AuthState>(
+                        listener: (context, state) {
+                          context
+                              .read<AccountPageBloc>()
+                              .add(AccountPageLoadEvent());
+                        },
+                        builder: (context, state) {
+                          return state.status == AuthStatus.authenticated
+                              ? BlocListener<LogoutCubit, LogoutState>(
+                                  listener: (context, state) {
+                                    if (state is LogoutSuccess) {
+                                      context
+                                          .read<AuthCubit>()
+                                          .loadAuthenticationState();
+                                    }
+                                  },
+                                  child: ListTile(
+                                    leading: CircleAvatar(
+                                      backgroundColor: Colors.blue,
+                                      child: Text(
+                                        "Jane Doe"[0],
+                                        style: const TextStyle(
+                                            color: Colors.white),
+                                      ),
+                                    ),
+                                    title: const Text("Jane Doe"),
+                                    subtitle:
+                                        const Text("jane.doe@example.com"),
+                                  ),
+                                )
+                              : Column(
+                                  children: [
+                                    Text(
+                                      SiteMessageConstants
+                                          .defalutMobileAppAccountUnauthenticatedDescription,
+                                    ),
+                                    const SizedBox(
+                                        height:
+                                            AppStyle.defaultVerticalPadding),
+                                    PrimaryButton(
+                                      child: const Text(
+                                          LocalizationConstants.signIn),
+                                      onPressed: () {
+                                        AppRoute.login
+                                            .navigateBackStack(context);
+                                      },
+                                    ),
+                                  ],
+                                );
+                        },
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: AppStyle.defaultVerticalPadding),
+                  ...buildContentWidgets(state.pageWidgets),
+                ],
+              ),
+            );
           case AccountPageFailureState():
             return const Center(
                 child: Text(LocalizationConstants.errorLoadingAccount));
