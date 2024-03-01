e0ccb6638855fc1850f323c0be883fd984397a4d
Tawhid Joarder <tawhid.joarder@optimizely.com>
2024-03-01 12:48:19 +0600

    product details add to cart refactored

--- lib/features/presentation/screens/product_details/product_details_add_to_cart_widget.dart
+++ lib/features/presentation/screens/product_details/product_details_add_to_cart_widget.dart
@@ -1,5 +1,6 @@
 import 'package:commerce_flutter_app/core/constants/app_route.dart';
 import 'package:commerce_flutter_app/core/constants/localization_constants.dart';
+import 'package:commerce_flutter_app/features/domain/entity/product_details/product_details_add_to_cart_entity.dart';
 import 'package:commerce_flutter_app/features/domain/enums/auth_status.dart';
 import 'package:commerce_flutter_app/features/presentation/bloc/auth/auth_cubit.dart';
 import 'package:commerce_flutter_app/features/presentation/bloc/product_details/product_details_add_to_cart_bloc/product_details_add_to_cart_bloc.dart';
@@ -43,6 +44,7 @@ class AddToCartSignInWidget extends StatelessWidget {
   final textEditingController = TextEditingController();
 
   AddToCartSignInWidget();
+
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<ProductDetailsAddToCartBloc,
@@ -52,70 +54,7 @@ class AddToCartSignInWidget extends StatelessWidget {
         case ProductDetailsAddtoCartLoading():
           return const Center(child: CircularProgressIndicator());
         case ProductDetailsAddtoCartSuccess():
-          var detailsAddToCartEntity = state.productDetailsAddToCartEntity;
-          return Container(
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 20.0),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        flex: 5,
-                        child: NumberTextField(
-                            initialtText: detailsAddToCartEntity.quantityText,
-                            onChanged: (int? quantity) {
-                              var pricingState = context
-                                  .read<ProductDetailsPricingBloc>()
-                                  .state;
-                              if (pricingState is ProductDetailsPricingLoaded) {
-                                var productDetailsPricingEntity =
-                                    pricingState.productDetailsPriceEntity;
-                                productDetailsPricingEntity =
-                                    productDetailsPricingEntity.copyWith(
-                                        quantity: quantity);
-                                context.read<ProductDetailsPricingBloc>().add(
-                                    LoadProductDetailsPricing(
-                                        productDetailsPriceEntity:
-                                            productDetailsPricingEntity));
-                              }
-                            }),
-                      ),
-                      Expanded(
-                        flex: 2,
-                        child: Column(
-                          children: [
-                            Text('U/M'),
-                            Text('E/A',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w500,
-                                    fontSize: 18.0))
-                          ],
-                        ),
-                      ),
-                      Expanded(
-                        flex: 2,
-                        child: Column(
-                          children: [
-                            Text('Subtotal'),
-                            Text(
-                              detailsAddToCartEntity?.subtotalValueText ?? '',
-                              style: TextStyle(
-                                  fontWeight: FontWeight.w500, fontSize: 18.0),
-                            )
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                PrimaryButton(
-                  child: const Text(LocalizationConstants.addToCart),
-                  onPressed: () {},
-                )
-              ],
-            ),
-          );
+          return AddToCartSuccessWidget(state.productDetailsAddToCartEntity);
         case ProductDetailsAddtoCartError():
           return Center(child: Text(state.errorMessage));
         default:
@@ -125,7 +64,88 @@ class AddToCartSignInWidget extends StatelessWidget {
   }
 }
 
-class QuantityTextChangeEvent {}
+class AddToCartSuccessWidget extends StatelessWidget {
+  final ProductDetailsAddtoCartEntity detailsAddToCartEntity;
+
+  AddToCartSuccessWidget(this.detailsAddToCartEntity);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          ProductDetailsAddCartRow(detailsAddToCartEntity),
+          PrimaryButton(
+            child: const Text(LocalizationConstants.addToCart),
+            onPressed: () {},
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ProductDetailsAddCartRow extends StatelessWidget {
+  final ProductDetailsAddtoCartEntity detailsAddToCartEntity;
+
+  ProductDetailsAddCartRow(this.detailsAddToCartEntity);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20.0),
+      child: Row(
+        children: [
+          Expanded(
+            flex: 5,
+            child: NumberTextField(
+                initialtText: detailsAddToCartEntity.quantityText,
+                onChanged: (int? quantity) {
+                  var pricingState =
+                      context.read<ProductDetailsPricingBloc>().state;
+                  if (pricingState is ProductDetailsPricingLoaded) {
+                    var productDetailsPricingEntity =
+                        pricingState.productDetailsPriceEntity;
+                    productDetailsPricingEntity = productDetailsPricingEntity
+                        .copyWith(quantity: quantity);
+                    context.read<ProductDetailsPricingBloc>().add(
+                        LoadProductDetailsPricing(
+                            productDetailsPriceEntity:
+                                productDetailsPricingEntity));
+                  }
+                }),
+          ),
+          ProductDetailsAddCartTtitleSubTitleColumn('U/M', 'E/A'),
+          ProductDetailsAddCartTtitleSubTitleColumn(
+              'Subtotal', detailsAddToCartEntity?.subtotalValueText ?? ''),
+        ],
+      ),
+    );
+  }
+}
+
+class ProductDetailsAddCartTtitleSubTitleColumn extends StatelessWidget {
+  final String title;
+  final String value;
+
+  ProductDetailsAddCartTtitleSubTitleColumn(this.title, this.value);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 2,
+      child: Column(
+        children: [
+          Text(title),
+          Text(
+            value,
+            style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18.0),
+          )
+        ],
+      ),
+    );
+  }
+}
 
 class AddToCartNotSignedInWidget extends StatelessWidget {
   @override
