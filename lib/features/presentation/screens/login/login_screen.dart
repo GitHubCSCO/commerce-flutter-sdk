322d7f1880e3ceababa5448e06f9e18bd02750f1
Mosabbir-Khan <mosabbir.khan@optimizely.com>
2024-03-06 13:56:24 +0600

    fix: login screen texts and background

--- lib/features/presentation/screens/login/login_screen.dart
+++ lib/features/presentation/screens/login/login_screen.dart
@@ -65,7 +65,6 @@ class _LoginPageState extends State<LoginPage> {
           ),
         ],
         automaticallyImplyLeading: false,
-        forceMaterialTransparency: true,
         bottom: PreferredSize(
           preferredSize: const Size.fromHeight(20),
           child: Container(
@@ -74,90 +73,109 @@ class _LoginPageState extends State<LoginPage> {
           ),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(height: 50),
-              Image.asset(
-                AssetConstants.logo,
-                width: 100,
-                height: 100,
-              ),
-              const SizedBox(height: 50),
-              Input(
-                label: LocalizationConstants.username,
-                hintText: LocalizationConstants.enterUsername,
-                controller: _usernameController,
-                onTapOutside: (context) =>
-                    FocusManager.instance.primaryFocus?.unfocus(),
-                onEditingComplete: () => FocusScope.of(context).nextFocus(),
-              ),
-              const SizedBox(height: 16.0),
-              Input(
-                label: LocalizationConstants.password,
-                hintText: LocalizationConstants.enterPassword,
-                obscureText: true,
-                controller: _passwordController,
-                onTapOutside: (context) =>
-                    FocusManager.instance.primaryFocus?.unfocus(),
-              ),
-              const SizedBox(height: 16.0),
-              BlocConsumer<LoginCubit, LoginState>(
-                listener: (context, state) {
-                  if (state is LoginSuccessState) {
-                    context.read<AuthCubit>().loadAuthenticationState();
+      body: Container(
+        height: double.infinity,
+        color: AppStyle.neutral00,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 50),
+                Image.asset(
+                  AssetConstants.logo,
+                  width: 100,
+                  height: 100,
+                ),
+                const SizedBox(height: 50),
+                Input(
+                  label: LocalizationConstants.username,
+                  hintText: LocalizationConstants.enterUsername,
+                  controller: _usernameController,
+                  onTapOutside: (context) =>
+                      FocusManager.instance.primaryFocus?.unfocus(),
+                  onEditingComplete: () => FocusScope.of(context).nextFocus(),
+                ),
+                const SizedBox(height: 16.0),
+                Input(
+                  label: LocalizationConstants.password,
+                  hintText: LocalizationConstants.enterPassword,
+                  obscureText: true,
+                  controller: _passwordController,
+                  onTapOutside: (context) =>
+                      FocusManager.instance.primaryFocus?.unfocus(),
+                ),
+                const SizedBox(height: 16.0),
+                BlocConsumer<LoginCubit, LoginState>(
+                  listener: (context, state) {
+                    if (state is LoginSuccessState) {
+                      context.read<AuthCubit>().loadAuthenticationState();
 
-                    if (state.showBiometricOptionView) {
-                      // Display biometric option view
-                      return;
+                      if (state.showBiometricOptionView) {
+                        // Display biometric option view
+                        return;
+                      }
+
+                      context.pop();
+                    } else if (state is LoginFailureState) {
+                      showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          title:
+                              state.title != null ? Text(state.title!) : null,
+                          content: state.message != null
+                              ? Text(state.message!)
+                              : null,
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: Text(state.buttonText ?? ''),
+                            ),
+                          ],
+                        ),
+                      );
                     }
-
-                    context.pop();
-                  } else if (state is LoginFailureState) {
-                    showDialog(
-                      context: context,
-                      builder: (context) => AlertDialog(
-                        title: state.title != null ? Text(state.title!) : null,
-                        content:
-                            state.message != null ? Text(state.message!) : null,
-                        actions: [
-                          TextButton(
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                            child: Text(state.buttonText ?? ''),
-                          ),
-                        ],
-                      ),
-                    );
-                  }
-                },
-                builder: (context, state) {
-                  if (state is LoginLoadingState) {
-                    return const CircularProgressIndicator(); // Display a loading indicator
-                  } else {
-                    return PrimaryButton(
-                      onPressed: () {
-                        FocusManager.instance.primaryFocus?.unfocus();
-                        BlocProvider.of<LoginCubit>(context).onLoginSubmit(
-                          _usernameController.text,
-                          _passwordController.text,
-                        );
-                      },
-                      text: LocalizationConstants.signIn,
-                    );
-                  }
-                },
-              ),
-              const SizedBox(height: 16.0),
-              const SecondaryButton(child: Text(LocalizationConstants.faceID)),
-              const SizedBox(height: 16.0),
-              const PlainButton(
-                  child: Text(LocalizationConstants.forgotPassword)),
-            ],
+                  },
+                  builder: (context, state) {
+                    if (state is LoginLoadingState) {
+                      return const CircularProgressIndicator(); // Display a loading indicator
+                    } else {
+                      return PrimaryButton(
+                        onPressed: () {
+                          FocusManager.instance.primaryFocus?.unfocus();
+                          BlocProvider.of<LoginCubit>(context).onLoginSubmit(
+                            _usernameController.text,
+                            _passwordController.text,
+                          );
+                        },
+                        text: LocalizationConstants.signIn,
+                      );
+                    }
+                  },
+                ),
+                const SizedBox(height: 16.0),
+                SecondaryButton(
+                  child: Text(
+                    LocalizationConstants.faceID,
+                    style: OptiTextStyles.subtitle.copyWith(
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16.0),
+                PlainButton(
+                  child: Text(
+                    LocalizationConstants.forgotPassword,
+                    style: OptiTextStyles.subtitle.copyWith(
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
