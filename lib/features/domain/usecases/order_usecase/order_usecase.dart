6254dfff1805c61612f3f244f34cf5ae48d67aa2
Mosabbir-Khan <mosabbir.khan@optimizely.com>
2024-03-23 12:56:31 +0600

    cleanup: unecessary comments

--- lib/features/domain/usecases/order_usecase/order_usecase.dart
+++ lib/features/domain/usecases/order_usecase/order_usecase.dart
@@ -6,35 +6,6 @@ import 'package:optimizely_commerce_api/optimizely_commerce_api.dart';
 
 class OrderUsecase extends BaseUseCase {
   Future<GetOrderCollectionResultEntity?> getOrderHistory({int? page}) async {
-    // TODO - put proper implementation
-    // return Future.delayed(
-    //   const Duration(seconds: 1),
-    //   () => [
-    //     OrderEntity(
-    //       webOrderNumber: 'WEB001022',
-    //       orderNumberLabel: '828288282',
-    //       customerPO: 'X76332',
-    //       stCompanyName: 'St. George',
-    //       orderDate: DateTime.tryParse('2021-07-01'),
-    //       orderGrandTotalDisplay: '\$14,000.00',
-    //       statusDisplay: 'Pending',
-    //       webOrderNumberLabel: 'Web Order #',
-    //       poNumberLabel: 'PO #',
-    //     ),
-    //     OrderEntity(
-    //       webOrderNumber: 'WEB001023',
-    //       orderNumberLabel: '828288283',
-    //       customerPO: 'X76333',
-    //       stCompanyName: 'St. George',
-    //       orderDate: DateTime.tryParse('2021-07-02'),
-    //       orderGrandTotalDisplay: '\$15,000.00',
-    //       statusDisplay: 'Pending',
-    //       webOrderNumberLabel: 'Web Order #',
-    //       poNumberLabel: 'PO #',
-    //     ),
-    //   ],
-    // );
-
     final sortOrders = commerceAPIServiceProvider
         .getOrderService()
         .getOrderListForRequest(OrderSortOrder.orderDateDescending);
