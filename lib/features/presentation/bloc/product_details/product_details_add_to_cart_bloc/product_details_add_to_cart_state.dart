e2a7b116a3543d010688e5cd9fb80b4b233abc9b
Tawhid Joarder <tawhid.joarder@optimizely.com>
2024-04-04 12:17:42 +0600

    ICM-4284

--- lib/features/presentation/bloc/product_details/product_details_add_to_cart_bloc/product_details_add_to_cart_state.dart
+++ lib/features/presentation/bloc/product_details/product_details_add_to_cart_bloc/product_details_add_to_cart_state.dart
@@ -1,13 +1,7 @@
-import 'dart:math';
-
 import 'package:commerce_flutter_app/features/domain/entity/product_details/product_details_add_to_cart_entity.dart';
-import 'package:equatable/equatable.dart';
 
-abstract class ProductDetailsAddtoCartState extends Equatable {
+abstract class ProductDetailsAddtoCartState {
   const ProductDetailsAddtoCartState();
-
-  @override
-  List<Object> get props => [];
 }
 
 class ProductDetailsAddtoCartInitial extends ProductDetailsAddtoCartState {}
@@ -24,24 +18,15 @@ class ProductDetailsAddtoCartError extends ProductDetailsAddtoCartState {
   final String errorMessage;
 
   const ProductDetailsAddtoCartError(this.errorMessage);
-
-  @override
-  List<Object> get props => [errorMessage];
 }
 
 // product added to cart
 class ProductDetailsProdctAddedToCartSuccess
-    extends ProductDetailsAddtoCartState {
-  @override
-  List<Object> get props => [Random().nextDouble()];
-}
+    extends ProductDetailsAddtoCartState {}
 
 class ProductDetailsProdctAddedToCartError
     extends ProductDetailsAddtoCartState {
   final String errorMessage;
 
   const ProductDetailsProdctAddedToCartError(this.errorMessage);
-
-  @override
-  List<Object> get props => [errorMessage];
 }
