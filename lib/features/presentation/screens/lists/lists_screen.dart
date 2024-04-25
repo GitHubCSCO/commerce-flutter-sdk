7e97f666d53f24ef868b58c8add88143088958c0
Mosabbir-Khan <mosabbir.khan@optimizely.com>
2024-04-25 12:36:05 +0600

    add: list screen search bar

--- lib/features/presentation/screens/lists/lists_screen.dart
+++ lib/features/presentation/screens/lists/lists_screen.dart
@@ -1,25 +1,72 @@
+import 'package:commerce_flutter_app/core/colors/app_colors.dart';
+import 'package:commerce_flutter_app/core/constants/asset_constants.dart';
+import 'package:commerce_flutter_app/core/constants/localization_constants.dart';
+import 'package:commerce_flutter_app/core/extensions/context.dart';
+import 'package:commerce_flutter_app/features/presentation/base/base_dynamic_content_screen.dart';
+import 'package:commerce_flutter_app/features/presentation/components/input.dart';
+import 'package:commerce_flutter_app/features/presentation/components/snackbar_coming_soon.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class ListsScreen extends StatelessWidget {
   const ListsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const ListsPage();
+    return ListsPage();
   }
 }
 
-class ListsPage extends StatelessWidget {
-  const ListsPage({super.key});
+class ListsPage extends BaseDynamicContentScreen {
+  ListsPage({super.key});
+
+  final _textEditingController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: OptiAppColors.backgroundGray,
       appBar: AppBar(
-        title: const Text('Lists'),
+        backgroundColor: OptiAppColors.backgroundWhite,
+        title: const Text(LocalizationConstants.lists),
+        centerTitle: false,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.more_vert),
+            onPressed: () => CustomSnackBar.showComingSoonSnackBar(context),
+          )
+        ],
       ),
-      body: const Center(
-        child: Text('Lists Page'),
+      body: Column(
+        children: [
+          Container(
+            color: Colors.white,
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+            child: Input(
+              hintText: LocalizationConstants.search,
+              suffixIcon: IconButton(
+                icon: SvgPicture.asset(
+                  AssetConstants.iconClear,
+                  semanticsLabel: 'search query clear icon',
+                  fit: BoxFit.fitWidth,
+                ),
+                onPressed: () {
+                  _textEditingController.clear();
+                  context.closeKeyboard();
+                },
+              ),
+              onTapOutside: (p0) => context.closeKeyboard(),
+              textInputAction: TextInputAction.search,
+              controller: _textEditingController,
+              onSubmitted: (value) {
+                CustomSnackBar.showComingSoonSnackBar(context);
+              },
+            ),
+          ),
+          const Center(
+            child: Text('List Screen'),
+          )
+        ],
       ),
     );
   }
