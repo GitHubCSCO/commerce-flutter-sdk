d3062db8054a42d8cb2193c65c113619f91a0842
Mosabbir-Khan <mosabbir.khan@optimizely.com>
2024-05-06 10:59:24 +0600

    add: real time pricing in wishlistlines

--- lib/features/domain/usecases/wish_list_usecase/wish_list_details_usecase.dart
+++ lib/features/domain/usecases/wish_list_usecase/wish_list_details_usecase.dart
@@ -1,6 +1,8 @@
 import 'package:commerce_flutter_app/core/constants/core_constants.dart';
 import 'package:commerce_flutter_app/features/domain/entity/wish_list/wish_list_entity.dart';
 import 'package:commerce_flutter_app/features/domain/entity/wish_list/wish_list_line_collection_entity.dart';
+import 'package:commerce_flutter_app/features/domain/entity/wish_list/wish_list_line_entity.dart';
+import 'package:commerce_flutter_app/features/domain/mapper/product_price_mapper.dart';
 import 'package:commerce_flutter_app/features/domain/mapper/wish_list_mapper.dart';
 import 'package:commerce_flutter_app/features/domain/usecases/base_usecase.dart';
 import 'package:optimizely_commerce_api/optimizely_commerce_api.dart';
@@ -39,11 +41,68 @@ class WishListDetailsUsecase extends BaseUseCase {
 
     switch (result) {
       case Success(value: final value):
-        return value != null
-            ? WishListLineCollectionEntityMapper.toEntity(value)
-            : null;
+        if (value == null) {
+          return null;
+        }
+
+        final valueEntity = WishListLineCollectionEntityMapper.toEntity(value);
+        final priceParameters = value.wishListLines
+                ?.map(
+                  (line) => ProductPriceQueryParameter(
+                    productId: line.productId,
+                    qtyOrdered: line.qtyOrdered,
+                    unitOfMeasure: line.unitOfMeasure,
+                  ),
+                )
+                .toList() ??
+            [];
+
+        if (priceParameters.isEmpty) {
+          return valueEntity;
+        }
+
+        final updatedLines = await loadWishListLinesWithRealTimePricing(
+          wishListLineColelction: valueEntity,
+          priceParameters: priceParameters,
+        );
+
+        return updatedLines;
+
       case Failure():
         return null;
+    }
+  }
+
+  Future<WishListLineCollectionEntity> loadWishListLinesWithRealTimePricing({
+    required WishListLineCollectionEntity wishListLineColelction,
+    required List<ProductPriceQueryParameter> priceParameters,
+  }) async {
+    final realTimePricesResult = await commerceAPIServiceProvider
+        .getRealTimePricingService()
+        .getProductRealTimePrices(
+          RealTimePricingParameters(
+            productPriceParameters: priceParameters,
+          ),
+        );
+
+    switch (realTimePricesResult) {
+      case Success(value: final realTimePrices):
+        final lines =
+            wishListLineColelction.wishListLines ?? <WishListLineEntity>[];
+        final newLines = lines.map((line) {
+          final realTimePrice = realTimePrices?.realTimePricingResults
+              ?.firstWhere((element) => element.productId == line.productId);
+
+          return line.copyWith(
+            pricing: realTimePrice != null
+                ? ProductPriceEntityMapper().toEntity(realTimePrice)
+                : null,
+          );
+        }).toList();
+
+        return wishListLineColelction.copyWith(wishListLines: newLines);
+      case Failure():
+        return wishListLineColelction;
     }
   }
 
