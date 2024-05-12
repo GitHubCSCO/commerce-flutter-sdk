a2ae0f581177936b0b0700c8aa14c49bce42c990
Mosabbir-Khan <mosabbir.khan@optimizely.com>
2024-05-12 18:30:33 +0600

    fix: only show add to cart if canAddAllToCart is true

--- lib/features/presentation/screens/wish_list/wish_list_details/wish_list_details_screen.dart
+++ lib/features/presentation/screens/wish_list/wish_list_details/wish_list_details_screen.dart
@@ -294,6 +294,7 @@ class _WishListDetailsPageState extends State<WishListDetailsPage> {
                       clipBehavior: Clip.antiAlias,
                       decoration: const BoxDecoration(color: Colors.white),
                       child: PrimaryButton(
+                        isEnabled: state.wishList.canAddAllToCart == true,
                         onPressed: () {
                           context
                               .read<WishListDetailsCubit>()
