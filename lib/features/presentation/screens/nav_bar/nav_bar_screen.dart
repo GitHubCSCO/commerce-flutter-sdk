cbe7e2376e5519d7d1d2074f44ee2e490c0a3349
Anisur Rahaman <>
2024-05-16 15:33:54 +0600

    cart change state management bug fix

--- lib/features/presentation/screens/nav_bar/nav_bar_screen.dart
+++ lib/features/presentation/screens/nav_bar/nav_bar_screen.dart
@@ -20,7 +20,7 @@ class NavBarScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<CartCountCubit, CartCountState>(
+    return BlocBuilder<CartCountCubit, CountState>(
         builder: (context, state) {
       return NavBarPage(
         navigationShell: navigationShell,
