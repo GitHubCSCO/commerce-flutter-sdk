1c7176c1ae86b338392df071b2a6ec745802e97b
Mosabbir-Khan <mosabbir.khan@optimizely.com>
2024-06-05 17:27:53 +0600

    add: "see more" if more than 8 items

--- lib/features/presentation/components/multiple_selection_option_chip.dart
+++ lib/features/presentation/components/multiple_selection_option_chip.dart
@@ -1,10 +1,13 @@
+import 'dart:math';
+
 import 'package:commerce_flutter_app/core/colors/app_colors.dart';
 import 'package:commerce_flutter_app/core/themes/theme.dart';
 import 'package:commerce_flutter_app/features/domain/entity/style_value_entity.dart';
+import 'package:commerce_flutter_app/features/presentation/components/buttons.dart';
 import 'package:commerce_flutter_app/features/presentation/components/filter.dart';
 import 'package:flutter/material.dart';
 
-class MultipleSelectionOptionChip<T> extends StatelessWidget {
+class MultipleSelectionOptionChip<T> extends StatefulWidget {
   const MultipleSelectionOptionChip({
     super.key,
     required this.values,
@@ -13,72 +16,130 @@ class MultipleSelectionOptionChip<T> extends StatelessWidget {
     this.chipTitle,
   });
 
+  static const maxItemsToShow = 8;
+
   final List<T> values;
   final Set<T> selectedValues;
   final String? chipTitle;
   final void Function(T? selection) onSelectionChanged;
 
-  // late Set<T> selectedValues;
+  bool get overFlow => values.length > maxItemsToShow;
+
+  @override
+  State<MultipleSelectionOptionChip<T>> createState() =>
+      _MultipleSelectionOptionChipState<T>();
+}
+
+class _MultipleSelectionOptionChipState<T>
+    extends State<MultipleSelectionOptionChip<T>> {
+  bool showAll = true;
+
+  @override
+  void initState() {
+    super.initState();
+    showAll = widget.overFlow ? false : true;
+
+    for (var value in widget.selectedValues) {
+      if (widget.values.indexOf(value) >=
+          MultipleSelectionOptionChip.maxItemsToShow) {
+        showAll = true;
+
+        continue;
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    List<T> itemsToShow = _getItemsToShow(values);
+    List<T> itemsToShow = _getItemsToShow(widget.values);
     return Padding(
-      padding: const EdgeInsets.fromLTRB(20.0, 10.0, 0.0, 0.0),
+      padding: const EdgeInsets.symmetric(horizontal: 10),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
-            chipTitle ?? '',
+            widget.chipTitle ?? '',
             style: OptiTextStyles.body,
           ),
           Wrap(
             spacing: 8,
             runSpacing: 2,
-            children: itemsToShow
-                .map(
-                  (value) => ChoiceChip(
-                    label: Text(
-                      _getValueTitle(value),
-                      style: selectedValues.contains(value)
-                          ? OptiTextStyles.bodySmallHighlight
-                              .copyWith(color: OptiAppColors.backgroundWhite)
-                          : OptiTextStyles.bodySmallHighlight,
-                    ),
-                    selectedColor: OptiAppColors.textPrimary,
-                    showCheckmark: false,
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 12,
-                      vertical: 6,
-                    ),
-                    selected: selectedValues.contains(value),
-                    onSelected: (bool selected) {
-                      if (!_getValueAvailibility(value)) {
-                        return;
-                      }
-                      // setState(
-                      //   () {
-                      //     selectedValues.contains(value)
-                      //         ? selectedValues.remove(value)
-                      //         : selectedValues.add(value);
-                      //   },
-                      // );
-                      onSelectionChanged(value);
-                    },
-                    backgroundColor: (!_getValueAvailibility(value))
-                        ? Colors.grey // Gray if not available
-                        : OptiAppColors.backgroundWhite,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(24),
-                      side: BorderSide(
-                        color: (!_getValueAvailibility(value))
-                            ? Colors.grey // Gray if not available
-                            : OptiAppColors.textPrimary,
-                        width: 1,
+            children: [
+              ...itemsToShow
+                  .map(
+                    (value) => ChoiceChip(
+                      label: Text(
+                        _getValueTitle(value),
+                        style: widget.selectedValues.contains(value)
+                            ? OptiTextStyles.bodySmallHighlight
+                                .copyWith(color: OptiAppColors.backgroundWhite)
+                            : OptiTextStyles.bodySmallHighlight,
+                      ),
+                      selectedColor: OptiAppColors.textPrimary,
+                      showCheckmark: false,
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 12,
+                        vertical: 6,
+                      ),
+                      selected: widget.selectedValues.contains(value),
+                      onSelected: (bool selected) {
+                        if (!_getValueAvailibility(value)) {
+                          return;
+                        }
+                        widget.onSelectionChanged(value);
+                      },
+                      backgroundColor: (!_getValueAvailibility(value))
+                          ? Colors.grey // Gray if not available
+                          : OptiAppColors.backgroundWhite,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24),
+                        side: BorderSide(
+                          color: (!_getValueAvailibility(value))
+                              ? Colors.grey // Gray if not available
+                              : OptiAppColors.textPrimary,
+                          width: 1,
+                        ),
                       ),
                     ),
+                  )
+                  .toList()
+                  .sublist(
+                    0,
+                    showAll
+                        ? itemsToShow.length
+                        : min(MultipleSelectionOptionChip.maxItemsToShow,
+                            itemsToShow.length),
                   ),
-                )
-                .toList(),
+              widget.overFlow
+                  ? (showAll
+                      ? PlainButton(
+                          onPressed: () {
+                            setState(() {
+                              showAll = false;
+                            });
+                          },
+                          child: Text(
+                            'See less',
+                            style: OptiTextStyles.bodySmallHighlight.copyWith(
+                              color: OptiAppColors.primaryColor,
+                            ),
+                          ),
+                        )
+                      : PlainButton(
+                          onPressed: () {
+                            setState(() {
+                              showAll = true;
+                            });
+                          },
+                          child: Text(
+                            'See more',
+                            style: OptiTextStyles.bodySmallHighlight.copyWith(
+                              color: OptiAppColors.primaryColor,
+                            ),
+                          ),
+                        ))
+                  : const SizedBox.shrink(),
+            ],
           ),
         ],
       ),
