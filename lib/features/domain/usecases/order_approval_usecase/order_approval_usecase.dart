dd39c35221b33f0269f81de7db87eb4b985b70cf
Mosabbir-Khan <mosabbir.khan@optimizely.com>
2024-06-12 14:03:47 +0600

    add: details screen logic in OrderApprovalUseCase

--- lib/features/domain/usecases/order_approval_usecase/order_approval_usecase.dart
+++ lib/features/domain/usecases/order_approval_usecase/order_approval_usecase.dart
@@ -1,4 +1,6 @@
 import 'package:commerce_flutter_app/core/constants/core_constants.dart';
+import 'package:commerce_flutter_app/core/extensions/result_extension.dart';
+import 'package:commerce_flutter_app/core/utils/inventory_utils.dart';
 import 'package:commerce_flutter_app/features/domain/usecases/base_usecase.dart';
 import 'package:optimizely_commerce_api/optimizely_commerce_api.dart';
 
@@ -14,11 +16,39 @@ class OrderApprovalUseCase extends BaseUseCase {
               ),
             );
 
-    switch (result) {
-      case Success(value: final value):
-        return value;
-      case Failure():
-        return null;
-    }
+    return result.getResultSuccessValue();
+  }
+
+  Future<Cart?> loadCart({required String cartId}) async {
+    final result = await commerceAPIServiceProvider
+        .getOrderService()
+        .getOrderApproval(cartId);
+
+    return result.getResultSuccessValue();
+  }
+
+  Future<bool> deleteOrder({required String cartId}) async {
+    final deleteResponse = await commerceAPIServiceProvider
+        .getCartService()
+        .deleteCart(cartId);
+
+    return deleteResponse.getResultSuccessValue() != null;
+  }
+
+  Future<bool> approveOrder({required Cart cart}) async {
+    cart.status = 'Cart';
+    final approveResponse =
+        await commerceAPIServiceProvider.getCartService().approveCart(cart);
+
+    return approveResponse.getResultSuccessValue() != null;
+  }
+
+  Future<bool> shouldShowWarehouseInventoryButton() async {
+    final productSettingsResult = await commerceAPIServiceProvider
+        .getSettingsService()
+        .getProductSettingsAsync();
+
+    final value = productSettingsResult.getResultSuccessValue();
+    return InventoryUtils.isInventoryPerWarehouseButtonShownAsync(value);
   }
 }
