564b1bda1e4ed2a84da2cba57c5f40586ae676e8
anisuroptimizely <anisur.rahaman@optimizely.com>
2024-08-02 17:03:04 +0600

    format: search_product_grid_item_widget.dart format to dart format

--- lib/features/presentation/widget/search_product/search_product_grid_item_widget.dart
+++ lib/features/presentation/widget/search_product/search_product_grid_item_widget.dart
@@ -65,8 +65,7 @@ class SearchProductGridItemWidget extends StatelessWidget {
                       StackTrace? stackTrace) {
                     // This function is called when the image fails to load
                     return Container(
-                      color:
-                          OptiAppColors.backgroundGray, // Placeholder color
+                      color: OptiAppColors.backgroundGray, // Placeholder color
                       alignment: Alignment.center,
                       child: const Icon(
                         Icons.image, // Icon to display
@@ -123,60 +122,6 @@ class SearchProductGridItemWidget extends StatelessWidget {
                   ],
                 ),
               ),
-              // BlocProvider<AddToCartCubit>(
-              //     create: (context) =>
-              //         sl<AddToCartCubit>()..updateAddToCartButton(product),
-              //     child: BlocListener<AddToCartCubit, AddToCartState>(
-              //         listener: (context, state) {
-              //           if (state is AddToCartSuccess) {
-              //             context.read<CartCountCubit>().onCartItemChange();
-              //             CustomSnackBar.showProductAddedToCart(context);
-              //           }
-              //         },
-              //         child: BlocBuilder<AddToCartCubit, AddToCartState>(
-              //           buildWhen: (previous, current) =>
-              //               current is AddToCartEnable &&
-              //               previous is AddToCartButtonLoading,
-              //           builder: (context, state) {
-              //             if (state is AddToCartInitial) {
-              //               return Container();
-              //             } else if (state is AddToCartButtonLoading) {
-              //               return Container(
-              //                 alignment: Alignment.bottomLeft,
-              //                 child: LoadingAnimationWidget.prograssiveDots(
-              //                   color: OptiAppColors.iconPrimary,
-              //                   size: 30,
-              //                 ),
-              //               );
-              //             } else if (state is AddToCartEnable) {
-              //               if (state.canAddToCart) {
-              //                 return InkWell(
-              //                   onTap: () {
-              //                     var productId =
-              //                         product.styleParentId ?? product.id;
-              //                     context
-              //                         .read<AddToCartCubit>()
-              //                         .searchPorductAddToCard(productId!);
-              //                   },
-              //                   child: Container(
-              //                     width: 40,
-              //                     height: 40,
-              //                     padding: const EdgeInsets.all(10),
-              //                     decoration: BoxDecoration(
-              //                       color: const Color(0xFFF5F5F5),
-              //                       borderRadius: BorderRadius.circular(32),
-              //                     ),
-              //                     child: const SvgAssetImage(
-              //                       assetName: AssetConstants.addToCart,
-              //                       fit: BoxFit.fitWidth,
-              //                     ),
-              //                   ),
-              //                 );
-              //               }
-              //             }
-              //             return Container();
-              //           },
-              //         )))
             ],
           ),
         ],
