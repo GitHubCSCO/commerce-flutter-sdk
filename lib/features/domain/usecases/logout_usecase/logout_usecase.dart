6655724291c7e547e2dce9202917cee317ab84a1
Mosabbir-Khan <mosabbir.khan@optimizely.com>
2024-08-09 19:00:12 +0600

    add: landing screen after logout

--- lib/features/domain/usecases/logout_usecase/logout_usecase.dart
+++ lib/features/domain/usecases/logout_usecase/logout_usecase.dart
@@ -1,6 +1,6 @@
-import 'package:commerce_flutter_app/features/domain/usecases/base_usecase.dart';
+import 'package:commerce_flutter_app/features/domain/usecases/domain_usecase/domain_usecase.dart';
 
-class LogoutUsecase extends BaseUseCase {
+class LogoutUsecase extends DomainUsecase {
   LogoutUsecase() : super();
 
   Future<void> logout() async {
