032a6e3d9d2cff433729b7a67832ef4e33d3f0ca
Mosabbir-Khan <mosabbir.khan@optimizely.com>
2024-08-15 18:39:49 +0600

    fix: process only last event of SearchTypingEvent

--- lib/features/presentation/bloc/search/search/search_bloc.dart
+++ lib/features/presentation/bloc/search/search/search_bloc.dart
@@ -1,3 +1,4 @@
+import 'package:bloc_concurrency/bloc_concurrency.dart';
 import 'package:commerce_flutter_app/core/extensions/result_extension.dart';
 import 'package:commerce_flutter_app/features/domain/usecases/search_usecase/search_usecase.dart';
 import 'package:commerce_flutter_app/features/presentation/screens/product/product_screen.dart';
@@ -21,7 +22,7 @@ class SearchBloc extends Bloc<SearchEvent, SearchState> {
         super(SearchCmsInitialState()) {
     on<SearchAutoCompleteLoadEvent>(_onSearchAutoCompleteLoadEvent);
     on<SearchFocusEvent>(_onSearchFocusEvent);
-    on<SearchTypingEvent>(_onSearchTypingEvent);
+    on<SearchTypingEvent>(_onSearchTypingEvent, transformer: restartable());
     on<SearchFieldPopulateEvent>(_onSearchFieldPopulateEvent);
     on<SearchUnFocusEvent>(_onSearchUnFocusEvent);
     on<SearchSearchEvent>(_onSearchSearchEvent);
