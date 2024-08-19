4ab0715a77e005c0a4e1ed255148090a28b295ca
Saiful Islam <saiful.islam@optimizely.com>
2024-08-19 15:34:25 +0600

    fix update pregen and postgen script

--- bricks/config_updater/hooks/post_gen.dart
+++ bricks/config_updater/hooks/post_gen.dart
@@ -1,3 +1,50 @@
-import 'package:mason/mason.dart';
+import 'dart:io';
 
-Future<void> run(HookContext context) async {}
+import 'package:mason/mason.dart';
+import 'package:path/path.dart' as path;
+import 'exceptions.dart';
+
+Future<void> run(HookContext context) async {
+  final logger = context.logger;
+  logger.info(
+    green.wrap(
+      'Executing postgen script',
+    ),
+  );
+  try {
+    final rootPath = Directory.current.path;
+    final libPath = path.join(rootPath, 'lib');
+    final pubSpecFile = File('$rootPath/pubspec.yaml');
+    if (await pubSpecFile.exists()) {
+      var config1 = path.join(libPath, 'core/config/custom_configuration.dart');
+      var config2 =
+          path.join(libPath, 'core/config/custom_configuration.g.dart');
+      final customConfigDart1 = File(config1);
+      final customConfigDart2 = File(config2);
+      if (await customConfigDart1.exists() &&
+          await customConfigDart2.exists()) {
+        logger.info(
+          green.wrap(
+            'Executing dart formatter',
+          ),
+        );
+        final buildRunnerProcess = await Process.start(
+          'dart',
+          ['format', config1, config2],
+          runInShell: true,
+          workingDirectory: rootPath,
+        );
+        await stdout.addStream(buildRunnerProcess.stdout);
+        await stderr.addStream(buildRunnerProcess.stderr);
+      }
+    } else {
+      throw RootFolderException();
+    }
+  } on RootFolderException catch (_) {
+    logger.alert(
+      red.wrap(
+        'Executing command from a different folder. Please execute from your project root.',
+      ),
+    );
+  }
+}
--- bricks/config_updater/hooks/pre_gen.dart
+++ bricks/config_updater/hooks/pre_gen.dart
@@ -6,12 +6,17 @@ import 'exceptions.dart';
 
 Future run(HookContext context) async {
   final logger = context.logger;
+  logger.info(
+    green.wrap(
+      'Executing pregen script',
+    ),
+  );
   try {
     final rootPath = Directory.current.path;
     final libPath = path.join(rootPath, 'lib');
     final customConfig = path.join(libPath, 'core/config/');
     final pubSpecFile = File('$rootPath/pubspec.yaml');
-    if (pubSpecFile.existsSync()) {
+    if (await pubSpecFile.exists()) {
       final brick = Brick.version(name: 'model', version: '0.9.1');
       Map<String, dynamic> variables = <String, dynamic>{
         'model_name': 'CustomConfiguration',
@@ -20,6 +25,16 @@ Future run(HookContext context) async {
         'jsonFile': './assets/config/custom_config.json',
         'on-conflict': 'overwrite',
       };
+      final customConfigDart1 =
+          File(path.join(libPath, 'core/config/custom_configuration.dart'));
+      if (await customConfigDart1.exists()) {
+        await customConfigDart1.delete();
+      }
+      final customConfigDart2 =
+          File(path.join(libPath, 'core/config/custom_configuration.g.dart'));
+      if (await customConfigDart2.exists()) {
+        await customConfigDart2.delete();
+      }
       final generator = await MasonGenerator.fromBrick(brick);
       final target = DirectoryGeneratorTarget(Directory(customConfig));
       await generator.hooks.preGen(
