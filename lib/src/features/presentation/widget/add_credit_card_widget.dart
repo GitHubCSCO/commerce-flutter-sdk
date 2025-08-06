604c72d3ea43a69d26759deb4186e77ee50aef46
anisuroptimizely <anisur.rahaman@optimizely.com>
2025-08-06 15:16:04 +0600

    fix: edit credit card

--- lib/src/features/presentation/widget/add_credit_card_widget.dart
+++ lib/src/features/presentation/widget/add_credit_card_widget.dart
@@ -119,7 +119,8 @@ class AddCreditCardPage extends StatelessWidget {
     required this.addCreditCardEntity,
   });
 
-  void _setUpEditCreditCardValues(BuildContext context) {
+  void _setUpEditCreditCardValues(
+      BuildContext context, WebsiteSettings? websiteSettings) {
     nameController.text =
         addCreditCardEntity.accountPaymentProfile?.cardHolderName ?? "";
     addressController.text =
@@ -127,6 +128,30 @@ class AddCreditCardPage extends StatelessWidget {
     cityController.text = addCreditCardEntity.accountPaymentProfile?.city ?? "";
     postalCodeController.text =
         addCreditCardEntity.accountPaymentProfile?.postalCode ?? "";
+
+    if (websiteSettings?.useSpreedlyDropIn == true) {
+      context.read<AddCreditCardBloc>().updateCreditCardInfo(
+          CreditCardInfoEntity(
+              cardIdentifier:
+                  addCreditCardEntity.accountPaymentProfile?.cardIdentifier ??
+                      '',
+              cardType:
+                  addCreditCardEntity.accountPaymentProfile?.cardType ?? '',
+              maskedCardNumber:
+                  addCreditCardEntity.accountPaymentProfile?.maskedCardNumber ??
+                      ''));
+    } else {
+      context.read<AddCreditCardBloc>().updateCreditCardInfo(
+          CreditCardInfoEntity(
+              cardIdentifier:
+                  addCreditCardEntity.accountPaymentProfile?.cardIdentifier ??
+                      '',
+              cardType:
+                  addCreditCardEntity.accountPaymentProfile?.cardType ?? '',
+              securityCode:
+                  addCreditCardEntity.accountPaymentProfile?.securityCode ??
+                      ''));
+    }
   }
 
   @override
@@ -179,7 +204,7 @@ class AddCreditCardPage extends StatelessWidget {
                 return const Center(child: CircularProgressIndicator());
               } else if (state is AddCreditCardLoadedState) {
                 if (!addCreditCardEntity.isAddNewCreditCard) {
-                  _setUpEditCreditCardValues(context);
+                  _setUpEditCreditCardValues(context, state.websiteSettings);
                   context.read<CardExpirationCubit>().onLoadCardExpirarionData(
                       addCreditCardEntity,
                       state.expirationYears,
