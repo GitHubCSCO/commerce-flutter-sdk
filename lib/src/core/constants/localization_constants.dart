42b0c6ccb20944529a0691b5ad2a65422c50375f
Mosabbir-Khan <mosabbir.khan@optimizely.com>
2025-10-06 16:50:31 +0600

    add: no options localization message

--- lib/src/core/constants/localization_constants.dart
+++ lib/src/core/constants/localization_constants.dart
@@ -302,6 +302,7 @@ enum LocalizationConstants {
   searchNoHistoryAvailable(keyword: "No Search History Available"),
   clearHistory(keyword: "Clear History"),
   searchNoResults(keyword: "Your search returned no results."),
+  noOptions(keyword: "No Options"),
   searchPrompt(keyword: "Search for products."),
   sort(keyword: "Sort"),
   resultsFor(keyword: "%s result(s) for '%s'"),
